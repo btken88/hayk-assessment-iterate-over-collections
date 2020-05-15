79b409d902c97e09cf9d8c3539ad3b4ec08ccbc3
github username <github email address>
2020-05-15 10:06:23 -0600

    changed the sort function

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Company
     attr_accessor :name, :size
 
@@ -9,8 +11,44 @@ end
 
 companies = [
     Company.new('Alpha', 30),
-    Company.new('Beta, 300),
-    Company.new('Delta, 3000)
+    Company.new('Beta', 300),
+    Company.new('Delta', 3000)
 ]
 
 ### Your code below this
+# Generate a list of strings with the name and size (eg. "Alpha - 30")
+def company_information_strings companies
+  companies.map { |company| "#{company.name} - #{company.size}"}
+end
+
+# Combine all the sizes with reduce
+def company_total_sizes companies
+  companies.reduce(0) { |total_size, company| total_size + company.size}
+end
+
+# Filter the list to show only companies over 100
+def companies_sized_over_100 companies
+  filtered_companies = companies.select { |company| company.size > 100 }
+  company_information_strings filtered_companies
+end
+
+# Find the company named "Beta"
+def find_company_by_name companies, name = "Beta"
+  companies.find {|company| company.name == name}
+end
+
+# Find the largest company
+def find_largest_company companies
+  companies.reduce() do |largest_company, company|
+    if company.size > largest_company.size
+      company
+    else
+      largest_company
+    end
+  end
+end
+
+# Sort the companies from largest to smallest
+def sort_companies companies
+  asdio
+end
